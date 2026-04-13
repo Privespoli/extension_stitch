328d28e968d60565908aacd727aaebbb29318398
Priscila Nunes <priscilavespoli92@gmail.com>
2026-04-13 17:03:45 +0200

    teste capi

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
 document.addEventListener('click', (e) => {
+  // Crear el elemento de imagen
   const img = document.createElement('img');
   
-  img.src = chrome.runtime.getURL('stitch.png');
+  // Obtener la ruta real de la imagen dentro de la extensión
+  img.src = chrome.runtime.getURL('capibara.png');
   
-  img.classList.add('stitch-click');
-  
-  // Posicionamento centralizado
-  img.style.left = `${e.pageX - 25}px`;
+  // Estilo para posicionarla donde hiciste clic
+  img.classList.add('capibara-click');
+  img.style.left = `${e.pageX - 25}px`; // Centrar imagen (asumiendo 50px ancho)
   img.style.top = `${e.pageY - 25}px`;
   
   document.body.appendChild(img);
 
-  // Remove do DOM após a animação acabar
-  setTimeout(() => img.remove(), 1500);
+  // Opcional: Eliminar la imagen después de 2 segundos
+  setTimeout(() => img.remove(), 2000);
 });
